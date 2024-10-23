ba1c8e6fa15992d9f05c841c4e2fd17885f35f2e
konga22 <qwopop2@gmail.com>
2024-10-23 10:30:28 +0900

    first commut

--- src/Question13.java
+++ src/Question13.java
@@ -3,17 +3,15 @@ import java.util.Scanner;
 public class Question13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int p;
-        int sp;
-        do{
-            System.out.println("처음 비밀번호 입력: ");
-            p = sc.nextInt();
-            System.out.println("다시 입력: ");
-            sp = sc.nextInt();
-            if(p != sp){
-                System.out.println("다시 입력해주세요");
-            }
-        }while(p != sp);
+
+        String password = "";
+
+        do {
+            System.out.println("암호를 입력해주세요: ");
+            password = sc.nextLine();
+            System.out.println(password);
+        } while(!password.equals("1234"));//스트링은 equals 값을 넣고 앞에 ! 을 넣어야함
+
         System.out.println("성공");
     }
 }
--- src/Question6.java
+++ src/Question6.java
@@ -6,6 +6,14 @@ public class Question6 {
         System.out.println("숫자 2개를 입력하셍: ");
         int n = sc.nextInt();
         int n2 = sc.nextInt();
-        int i = 0;
+        while (n2 != 0) {
+            int tmp = n2;
+            System.out.println("tmp=" + tmp);
+            n2 = n % n2;
+            System.out.println("n2= " + n2);
+            n = tmp;
+            System.out.println("n= " + n);
+        }
+        System.out.println("최대공약수: " + n);
     }
 }
